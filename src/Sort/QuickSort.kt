6c5ef1e28aaa7d548863a3dd748a42928c209fcc
brent <jinhoshin319@gmail.com>
2020-07-05 15:28:22 +0900

    quicksort done

--- src/Sort/QuickSort.kt
+++ src/Sort/QuickSort.kt
@@ -14,24 +14,19 @@ fun performQuickSort(arr: IntArray, idxLow: Int, idxHigh: Int) {
 private fun getLastPivotIndex(arr: IntArray, idxLow: Int, idxHigh: Int): Int {
     var lastIdxSmallerThanPivot = idxLow - 1
     if (idxHigh > idxLow) {
-        val pivot = arr.copyOfRange(idxLow, idxHigh + 1).random()
-        println("pivot : $pivot")
+        val idxPivot = Random.nextInt(idxLow, idxHigh + 1)
+        val temp = arr[idxPivot]
+        arr[idxPivot] = arr[idxHigh]
+        arr[idxHigh] = temp
         for (num in idxLow..idxHigh) {
-            if (arr[num] < pivot) {
+            if (arr[num] <= temp) {
                 lastIdxSmallerThanPivot++
-                var temp = arr[num]
+                val temp = arr[num]
                 arr[num] = arr[lastIdxSmallerThanPivot]
                 arr[lastIdxSmallerThanPivot] = temp
             }
-            print("$num  ")
         }
-        println()
-        println("indexLow : $idxLow, indexHigh : $idxHigh, lastIdxSmallerThanPivot: $lastIdxSmallerThanPivot,")
-        for (i in arr) {
-            print("$i  " )
-        }
-        println("recur--------------")
-        return arr.indexOf(pivot)
+        return lastIdxSmallerThanPivot ++
     }
     return 0
 }
