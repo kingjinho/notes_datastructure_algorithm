930179adafd2d334de6c303b5c501ac93d8515c2
kingjinho <jinhoshin319@gmail.com>
2020-07-09 08:54:37 +0900

    a simple change in calling heap sort method

--- src/main.kt
+++ src/main.kt
@@ -1,13 +1,15 @@
+import Sort.performHeapSort
 import Sort.performMergeSort
 import Sort.performQuickSort
 import Sort.performSelectionSort
 
 fun main() {
     var initialArr = intArrayOf(1,5,9,4,3,8)
-    performQuickSort(initialArr, 0, initialArr.size - 1)
+    val lastIndex = initialArr.lastIndex
+    performHeapSort(initialArr, if(lastIndex % 2 == 1) lastIndex else lastIndex - 1)
     println("result -----")
-   /* for (i in initialArr) {
+    for (i in initialArr) {
         println(i)
-    }*/
+    }
 
 }
