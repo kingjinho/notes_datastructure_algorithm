8415322d921dd5d4ff6f175b46e3a9619c6f159f
brent <jinhoshin319@gmail.com>
2020-07-20 21:18:18 +0900

    initial commit

--- src/Sort/InsertionSort.kt
+++ src/Sort/InsertionSort.kt
@@ -1,1 +1,14 @@
 package Sort
+
+fun performInsertionSort(arr: IntArray) {
+    for (i in 1 until arr.size) {
+        for (j in (i - 1) downTo 1) {
+            if (arr[j - 1] > arr[j]) {
+                val temp = arr[j - 1]
+                arr[j - 1] = arr[j]
+                arr[j] = temp
+            } else
+                continue
+        }
+    }
+}
