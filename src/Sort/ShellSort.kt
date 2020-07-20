ec849b0b3ff6197652093987331d01987d3dda90
brent <jinhoshin319@gmail.com>
2020-07-20 21:19:15 +0900

    ShellSort WIP

--- src/Sort/ShellSort.kt
+++ src/Sort/ShellSort.kt
@@ -1,2 +1,19 @@
 package Sort
 
+fun performShellSort(arr: IntArray, distance: Int) {
+
+    if (distance == 1)
+        performInsertionSort(arr)
+    else {
+        for (i in arr.indices step distance) {
+            if (i - distance < 0 || i + distance <= arr.size - 1) {
+                if (arr[i] > arr[i + distance]) {
+                    val temp = arr[i]
+                    arr[i] = arr[i + distance]
+                    arr[i + distance] = temp
+                }
+            }
+        }
+        performShellSort(arr, (distance / 2) + 1)
+    }
+}
