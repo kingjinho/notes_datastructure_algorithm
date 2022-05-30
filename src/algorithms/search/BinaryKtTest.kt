c5ba8d831223d1add3de09fa91fef74f04454ef1
brent <jinhoshin319@gmail.com>
2022-05-30 11:44:06 +0900

    add test on binary search recursive

---
+++ src/algorithms/search/BinaryKtTest.kt
@@ -0,0 +1,23 @@
+package algorithms.search
+
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class BinaryKtTest {
+
+    private lateinit var array: Array<Int>
+
+    @Before
+    fun setup() {
+        array = arrayOf(1,2,3,4,5,6,7,8,9)
+    }
+
+    @Test
+    fun binarySearchTest() {
+        val indexOfFive = binarySearchRecursive(array, 0, array.size - 2, 5)
+        assert(indexOfFive == 4)
+    }
+
+}
