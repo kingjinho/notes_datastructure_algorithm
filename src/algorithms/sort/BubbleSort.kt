b7aae818edcf4cd8a4ab791a129e28ded4b2a1e0
brent <jinhoshin319@gmail.com>
2022-06-01 11:27:50 +0900

    implement bubble sort for both desc and asc

--- src/algorithms/sort/BubbleSort.kt
+++ src/algorithms/sort/BubbleSort.kt
@@ -1,1 +1,27 @@
 package algorithms.sort
+
+fun bubbleSortAsc(array: IntArray): IntArray {
+    for (i in array.indices) {
+        for (j in 0 until array.size - i - 1) {
+            if(array[j] > array[j + 1]) {
+                val temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+            }
+        }
+    }
+    return array
+}
+
+fun bubbleSortDesc(array: IntArray): IntArray {
+    for (i in array.lastIndex downTo 0) {
+        for (j in array.lastIndex - i downTo 1) {
+            if(array[j-1] < array[j]) {
+                val temp = array[j-1]
+                array[j-1] = array[j]
+                array[j] = temp
+            }
+        }
+    }
+    return array
+}
