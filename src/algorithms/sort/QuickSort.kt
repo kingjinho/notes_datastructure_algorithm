e223a76551c077f3e3a0ec87aacbd9d9af1c9c3f
kingjinho <jinhoshin319@gmail.com>
2022-06-07 09:00:09 +0900

    add implementation of quick sort

--- src/algorithms/sort/QuickSort.kt
+++ src/algorithms/sort/QuickSort.kt
@@ -2,62 +2,30 @@ package algorithms.sort
 
 import kotlin.random.Random
 
-//1,2,3,4,5,6,7,8,9
-fun performSumWithDivideAndConquer(arr: IntArray): Int {
-    return if (arr.size == 1) {
-        arr[0]
-    } else {
-        arr[0] + performSumWithDivideAndConquer(arr.copyOfRange(1, arr.size))
-    }
-}
-
-fun performArraySizeWithDivideAndConquer(arr: IntArray): Int {
-    return if (arr.size == 1) {
-        1
-    } else {
-        1 + performArraySizeWithDivideAndConquer(arr.copyOfRange(1, arr.size))
-    }
-}
-//find a maximum number in a list
-
-
-//intArrayOf(1,5,9,4,3,8)
-fun performQuickSortOld(arr: IntArray, idxLow: Int, idxHigh: Int) {
-    if (idxHigh > idxLow) {
-        val idxPivot = getLastPivotIndex(arr, idxLow, idxHigh)
-        performQuickSortOld(arr, idxLow, idxPivot - 1)
-        performQuickSortOld(arr, idxPivot + 1, idxHigh)
-    }
-}
-
-private fun getLastPivotIndex(arr: IntArray, idxLow: Int, idxHigh: Int): Int {
-    var lastIdxSmallerThanPivot = idxLow - 1
-    if (idxHigh > idxLow) {
-        val idxPivot = Random.nextInt(idxLow, idxHigh + 1)
-        val valueAtPivotIdx = arr[idxPivot]
-        arr[idxPivot] = arr[idxHigh]
-        arr[idxHigh] = valueAtPivotIdx
-        for (num in idxLow..idxHigh) {
-            if (arr[num] <= valueAtPivotIdx) {
-                lastIdxSmallerThanPivot++
-                val temp = arr[num]
-                arr[num] = arr[lastIdxSmallerThanPivot]
-                arr[lastIdxSmallerThanPivot] = temp
-            }
-        }
-        return lastIdxSmallerThanPivot++
-    }
-    return 0
-}
-
-fun performQuicksortNew(arr: IntArray): IntArray {
-    if (arr.size >= 2) {
-        val pivotIndex = Random.nextInt(0, arr.size)
-        val lt = arr.filter { x -> arr.indexOf(x) != pivotIndex && x < arr[pivotIndex] }.toIntArray()
-        val gte = arr.filter { x -> arr.indexOf(x) != pivotIndex && x >= arr[pivotIndex] }.toIntArray()
-        val pivot = arr.filter { x -> arr.indexOf(x) == pivotIndex }.toIntArray()
-        return performQuicksortNew(lt) + pivot + performQuicksortNew(gte)
+fun performQuickSortOld(arr: IntArray, left: Int, right: Int): IntArray {
+    if (right > left) {
+        val idxPivot = partitionLast(arr, left, right)
+        performQuickSortOld(arr, left, idxPivot - 1)
+        performQuickSortOld(arr, idxPivot + 1, right)
     }
     return arr
+}
 
+private fun partitionLast(arr: IntArray, left: Int, right: Int): Int {
+    var i = left - 1
+    val pivot = arr[right]
+    for (num in left..right) {
+        if (arr[num] < pivot) {
+            i++
+            val temp = arr[num]
+            arr[num] = arr[i]
+            arr[i] = temp
+        }
+    }
+
+    i++
+    val temp = arr[right]
+    arr[right] = arr[i]
+    arr[i] = temp
+    return i
 }
